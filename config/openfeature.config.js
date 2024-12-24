db16889270b4dd4ffb8e1b0bcf3fb8b84a89aa6d
Ayan Upadhaya <ayanu881@gmail.com>
2024-12-24 14:18:02 +0600

    added with custom hook

--- config/openfeature.config.js
+++ config/openfeature.config.js
@@ -3,13 +3,48 @@ const { CustomProvider } = require("../provider/Provider");
 
 let openFeature;
 
+// Mock analytics system
+const sendToAnalytics = (data) => {
+  console.log("Sending data to analytics:", data);
+};
+
+//hooks are for monitoring
+class LoggingHook {
+  before(hookContext) {
+    console.log(
+      `Open feature initialized with custom provider of flag: ${hookContext.flagKey}`
+    );
+  }
+
+  after(hookContext, flagValue) {
+    console.log(
+      `[Tracking] Evaluation complete: ${hookContext.flagKey} = ${flagValue?.value}`
+    );
+
+    // Send evaluation data to analytics
+    sendToAnalytics({
+      feature: hookContext.flagKey,
+      value: flagValue?.value,
+      userContext: hookContext.context,
+      timestamp: new Date().toISOString(),
+    });
+  }
+  error(hookContext, error) {
+    console.error(
+      `[Tracking] Error evaluating feature: ${hookContext.flagKey}`,
+      error
+    );
+  }
+}
+// global Register the custom hook
+// OpenFeature.addHooks(new LoggingHook());
+
 const initializeOpenFeature = async () => {
   if (!openFeature) {
     client = OpenFeature.getClient();
     await OpenFeature.setProviderAndWait(new CustomProvider());
-    client.addHandler(ProviderEvents.Ready, async (eventDetails) => {
-      console.log("Open feature initialized with custom provider");
-    });
+    //registering hook for specific client
+    client.addHooks(new LoggingHook());
   }
 
   return client;
